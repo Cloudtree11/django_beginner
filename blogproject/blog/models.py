e541443fe69cb19a612ae9a5f3996a502ab52dea
Cloudtree11 <yunkun233@gmail.com>
2018-03-21 17:22:55 +0800

    Add blog detail project page implement

--- blogproject/blog/models.py
+++ blogproject/blog/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 from django.contrib.auth.models import User
 from django.utils.six import python_2_unicode_compatible
+from django.urls import reverse
 
 @python_2_unicode_compatible
 class Category(models.Model):
@@ -32,3 +33,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:detail', kwargs={'pk': self.pk})
+
