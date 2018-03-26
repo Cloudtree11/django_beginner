d36e7b5ee8a5c524d9a046f631cdb75d7c494f11
Cloudtree11 <yunkun233@gmail.com>
2018-03-26 14:39:40 +0800

    Blog project - fix littile problems

--- blogproject/blog/models.py
+++ blogproject/blog/models.py
@@ -5,6 +5,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils.six import python_2_unicode_compatible
 from django.urls import reverse
+import markdown
+from django.utils.html import strip_tags
 
 @python_2_unicode_compatible
 class Category(models.Model):
@@ -30,10 +32,30 @@ class Post(models.Model):
     category = models.ForeignKey(Category)
     tags = models.ManyToManyField(Tag, blank=True)
     author = models.ForeignKey(User)
+    views = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         return reverse('blog:detail', kwargs={'pk': self.pk})
+
+    class Meta:
+        ordering = ['-created_time']
+
+    def increase_views(self):
+        self.views += 1
+        self.save(update_fields=['views'])
+
+    def save(self, *args, **kwargs):
+        if not self.excerpt:
+            md = markdown.Markdown(extensions=[
+                'markdown.extensions.extra',
+                'markdown.extensions.codehilite',
+            ])
+            self.excerpt = strip_tags(md.convert(self.body))[:54]
+
+        super(Post, self).save(*args, **kwargs)
+    
+
 
--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -10,11 +10,12 @@ import markdown
 from comments.forms import CommentForm
 
 def index(request):
-    post_list = Post.objects.all().order_by('-created_time')
+    post_list = Post.objects.all()
     return render(request, 'blog/index.html', context={'post_list': post_list})
 
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
+    post.increase_views()
     post.body = markdown.markdown(post.body,
             extensions=[
                 'markdown.extensions.extra',
@@ -34,10 +35,12 @@ def detail(request, pk):
 def archives(request, year, month):
     post_list = Post.objects.filter(created_time__year=year,
             created_time__month=month,
-            ).order_by('-created_time')
+            )
     return render(request, 'blog/index.html', context={'post_list': post_list})
 
 def category(request, pk):
     cate = get_object_or_404(Category, pk=pk)
-    post_list = Post.objects.filter(category=cate).order_by('-created_time')
+    post_list = Post.objects.filter(category=cate)
     return render(request, 'blog/index.html', context={'post_list': post_list})
+
+
