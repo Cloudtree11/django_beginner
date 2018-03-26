a708ef4ae6575734c6af6c09a30189a3c3c7235a
Cloudtree11 <yunkun233@gmail.com>
2018-03-26 09:56:00 +0800

    Add blog project CommentForm feature

--- blogproject/comments/views.py
+++ blogproject/comments/views.py
@@ -9,19 +9,19 @@ from .forms import CommentForm
 def post_comment(request, post_pk):
     post = get_object_or_404(Post, pk=post_pk)
 
-    if request.method == 'Post':
+    if request.method == 'POST':
         form = CommentForm(request.POST)
 
         if form.is_valid():
             comment = form.save(commit=False)
             comment.post = post
             comment.save()
-            return redict(post)
+            return redirect(post)
         else:
             comment_list = post.comment_set.all()
             context = {'post': post,
                     'form': form,
                     'comment_list': comment_list,
                     }
-            return render(request, 'blog/detail.html', context=context)
+            return render(request, 'blog/detail.html', context)
     return redirect(post)
