ce432de897868a13a06afb864dc406dd306bfd1f
Cloudtree11 <yunkun233@gmail.com>
2018-03-26 17:48:10 +0800

    Blog project - Class-based views

--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -8,6 +8,7 @@ from django.http import HttpResponse
 from .models import Post, Category
 import markdown
 from comments.forms import CommentForm
+from django.views.generic import ListView, DetailView
 
 def index(request):
     post_list = Post.objects.all()
@@ -42,5 +43,70 @@ def category(request, pk):
     cate = get_object_or_404(Category, pk=pk)
     post_list = Post.objects.filter(category=cate)
     return render(request, 'blog/index.html', context={'post_list': post_list})
+
+class IndexView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
+
+class CategoryView(IndexView):
+    def get_queryset(self):
+        cate = get_object_or_404(Category, pk=self.kwargs.get('pk'))
+        return super(CategoryView, self).get_queryset().filter(category=cate)
+
+class ArchivesView(IndexView):
+    def get_queryset(self):
+        return super(ArchivesView, self).get_queryset().filter(
+                created_time__year=self.kwargs.get('year'),
+                created_time__month=self.kwargs.get('month')
+                )
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/detail.html'
+    context_object_name = 'post'
+
+    def get(self, request, *args, **kwargs):
+        response = super(PostDetailView, self).get(request, *args, **kwargs)
+        self.object.increase_views()
+        return response
+
+    def get_object(self, queryset=None):
+        post = super(PostDetailView, self).get_object(queryset=None)
+        post.body = markdown.markdown(post.body,
+                extensions=[
+                    'markdown.extensions.extra',
+                    'markdown.extensions.codehilite',
+                    'markdown.extensions.toc',
+                ])
+        return post
+
+    def get_context_data(self, **kwargs):
+        context = super(PostDetailView, self).get_context_data(**kwargs)
+        form = CommentForm()
+        comment_list = self.object.comment_set.all()
+        context.update({
+            'form': form,
+            'comment_list': comment_list,
+        })
+        return context
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
